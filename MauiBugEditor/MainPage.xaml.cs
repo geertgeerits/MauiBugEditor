a8c3b2ddea9034e501db971e978c0e2a268fb94b
Geert Geerits <ggeerits@hotmail.com>
2024-06-10 09:16:41 +0200

    Add project files.

---
+++ MauiBugEditor/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiBugEditor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            edtOcrResult.Text = "AAAAA-BBBBB CCCCC-DDDDD EEEEE-FFFFF GGGGG-HHHHH IIIII-JJJJJ KKKKK-LLLLL MMMMM-NNNNN OOOOO-PPPPP QQQQQ-RRRRR SSSSS-TTTTT UUUUU-VVVVV WWWWW-XXXXX YYYYY-ZZZZZ Welcome to .NET Multi-platform App UI";
+
+        }
+    }
+
+}
